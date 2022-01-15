e7015a54db518b85d8766c7604970cdfe43ac837
sinchan251 <farhanulrock1@gmail.com>
2022-01-15 13:36:08 +0700

    Create webhook FB messenger

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 import express from "express";
+import chatBotController from "../controller/chatBotController";
 import homePageController from "../controller/homepageController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
   router.get("/", homePageController.getHomePage);
+  router.get("/webhook", chatBotController.getWebHook);
+  router.post("/webhook", chatBotController.postWebHook);
 
   return app.use("/", router);
 };
